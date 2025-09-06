3a7e1c28f6609e637d18951e4db662c1bb8741ba
ivanYopolo <iyopolo@frba.utn.edu.ar>
2025-09-06 14:45:35 +0000

    Archivos 01 - Corregido y mejorado por legibilidad.

--- Y_01/Info_01/Practicas/2013/11-Archivos/11.1-Contar_palabras/includes.c
+++ Y_01/Info_01/Practicas/2013/11-Archivos/11.1-Contar_palabras/includes.c
@@ -15,30 +15,30 @@
 // Agarra un string del Kernel y lo guarda en un string.
 // ### DINÁMICO ###
 char * writeStrD(){
-    char strI[ TAM_STR ];
-    
-    // No poner strlen() porque toma el salto de línea.
-	while( fgets( strI, TAM_STR, stdin ) == NULL ){
-        printf( "[ ERROR: POR FAVOR, ESCRIBA DE VUELTA. ]\n" );
-    }	// ERROR
-	
-	if( strI[ strlen( strI ) - 1 ] != '\n' ){
-        // Flushea el buffer:
-		int ch = 0;
-		while( ( ch = getchar() ) != '\n' && ch != EOF );
-	}
-    
-	// Si se detecta un salto de línea:
-	if( strI[ strlen( strI ) - 1 ] == '\n' ){
-        strI[ strlen( strI ) - 1 ] = '\0';
-    }
-    
-    // Aloja memoria en el HEAP para el string.
-    char *strO = (char *) calloc( strlen( strI ), sizeof(char) );
-    
-    strcpy( strO, strI );
-    
-    return strO;
+   char strI[ TAM_STR ];
+
+   // No poner strlen() porque toma el salto de línea.
+   while( fgets( strI, TAM_STR, stdin ) == NULL ){
+      printf( "[ ERROR: POR FAVOR, ESCRIBA DE VUELTA. ]\n" );
+   }	// ERROR
+
+   if( strI[ strlen( strI ) - 1 ] != '\n' ){
+      // Flushea el buffer:
+      int ch = 0;
+      while( ( ch = getchar() ) != '\n' && ch != EOF );
+   }
+
+   // Si se detecta un salto de línea:
+   if( strI[ strlen( strI ) - 1 ] == '\n' ){
+      strI[ strlen( strI ) - 1 ] = '\0';
+   }
+
+   // Aloja memoria en el HEAP para el string.
+   char *strO = (char *) calloc( strlen( strI ), sizeof(char) );
+
+   strcpy( strO, strI );
+
+   return strO;
 }
 
 
@@ -48,43 +48,47 @@ char * writeStrD(){
 // Cuenta la cantidad de palabras en el archivo (Byte a Byte).
 // Devuelve NULL.
 char * contarPalabras( int fdi, int *cantidadPalabras ){
-    int cant = 1;
-    char buf[ 1 ];
-    char espacioBlanco = 0;
+   int cant = 1;
+   char buf[ 1 ];
+   char espacioBlanco = 0;
 
-    cant = read( fdi, buf, 1 );   // cant = Bytes leídos.
-    
-    if( !isspace( *buf ) )
-        (*cantidadPalabras)++;
-    
-    while( ( cant > 0 ) ){ // Cuenta la cantidad de palabras encontradas.
-        printf( "%c", *buf );
-        
-        // Se detecta un espacio en blanco.
-        if( ( !espacioBlanco ) && ( isspace( *buf ) ) ){
-            espacioBlanco = 1;
-        }
+   int fdlog = open( "conteoPalabras.log", O_WRONLY | O_TRUNC | O_CREAT, 0666 );
 
-        // Se dejó de detectar espacio en blanco: comienza una palabra.
-        if( ( espacioBlanco ) && ( !isspace( *buf ) ) ){
-            espacioBlanco = 0;
-            (*cantidadPalabras)++;
-        }
-            
-        // printf( "cantidadPalabras = %d\n", *cantidadPalabras );
-        
-        cant = read( fdi, buf, 1 );   // cant = Bytes leídos.
-    } // Llegó al final del archivo.
-    
-    return NULL;
+   cant = read( fdi, buf, 1 );   // cant = Bytes leídos.
+
+   if( !isspace( *buf ) )
+      (*cantidadPalabras)++;
+
+   while( ( cant > 0 ) ){ // Cuenta la cantidad de palabras encontradas.
+      // printf( "%c", *buf );
+      dprintf( fdlog, "%c", *buf );
+
+      // Se detecta un espacio en blanco.
+      if( ( !espacioBlanco ) && ( isspace( *buf ) ) ){
+         espacioBlanco = 1;
+      }
+
+      // Se dejó de detectar espacio en blanco: comienza una palabra.
+      if( ( espacioBlanco ) && ( !isspace( *buf ) ) ){
+         espacioBlanco = 0;
+         (*cantidadPalabras)++;
+      }
+
+      cant = read( fdi, buf, 1 );   // cant = Bytes leídos.
+   } // Llegó al final del archivo.
+   
+   dprintf( fdlog, "\n\n[ Se registró una cantidad de palabras de:\t %d ]", *cantidadPalabras );
+   
+   close( fdlog );
+
+   return NULL;
 }
 
 
 //--------------------------------------------------------------------------
 //                           contarPalabraClave
 //--------------------------------------------------------------------------
-// Cuenta la cantidad de palabras CLAVE en el archivo 
-// (varios Bytes/lectura).
+// Cuenta la cantidad de palabras CLAVE en el archivo (varios Bytes/lectura).
 // Dicha palabra la obtiene como input del usuario.
 // Devuelve la palabra clave dinámicamente.
 char * contarPalabraClave( int fdi, int *cantidadPalabras ){
@@ -94,19 +98,20 @@ char * contarPalabraClave( int fdi, int *cantidadPalabras ){
    long longitudClave = 0;     // Longitud del string clave.
    
    // Deja los datos en un archivo de LOG (tipo debugging).
-   int fdlog = open( "dump.log", O_WRONLY | O_TRUNC | O_CREAT, 0666 );
+   int fdlog = open( "conteoPalabraClave.log", O_WRONLY | O_TRUNC | O_CREAT, 0666 );
 
    printf( "Ingrese la palabra clave a buscar:\t\t" );
    palabraClave = writeStrD();
 
    longitudClave = strlen( palabraClave );   // Longitud del string.
    buf = calloc( longitudClave + TAM_STR, sizeof(char) );   // Tamaño dependiente del string clave.
-   cant = read( fdi, buf, longitudClave + TAM_STR );   // cant = Bytes leídos.
+   cant = read( fdi, buf, longitudClave + TAM_STR - 1 );   // cant = Bytes leídos.
    
-   buf[ cant - 1 ] = '\0';
+   buf[ cant ] = '\0';
    
    dprintf( fdlog, "[ BUF tiene tamaño de: %ld + %d = %ld B. ]\n", longitudClave, TAM_STR, longitudClave + TAM_STR );
-   dprintf( fdlog, "[ \'\\0\' ubicado en índice: %d ]\n\n", cant - 1 );
+   dprintf( fdlog, "[ \'\\0\' ubicado en índice: %d ]\n", cant );
+   dprintf( fdlog, "[ Mensaje clave a buscar:   \"%s\". ]\n\n", palabraClave );
 
    while ( cant > 0 && palabraClave != NULL ){
    
@@ -119,17 +124,19 @@ char * contarPalabraClave( int fdi, int *cantidadPalabras ){
       }  // Sale si es NULL (no encontró coincidencias).
       
       // Lo guarda en un archivo de log para analizar el texto.
-      dprintf( fdlog, "%ld -- %s\n", strlen( buf ), buf );
+      dprintf( fdlog, "[ Lectura %d B ] -- %s\n\n", cant, buf );
 
       // Guarda los últimos bytes correspondientes a longitudClave en las primeras posiciones.
       for ( int cursorTemp = 0; cursorTemp < longitudClave; cursorTemp++ ){
-         
+         // Empieza a copiar a partir de la longitud mínima del buffer.
          buf[ cursorTemp ] = buf[ TAM_STR + cursorTemp - 1 ];
       }
         
       // Se saltea los primeros Bytes (anteriores/antiguos).
       cant = read( fdi, buf + longitudClave, strlen( buf ) - longitudClave );   // cant = Bytes leídos.
    }  // Llegó al final del archivo.
+   
+   dprintf( fdlog, "\n\n[ Se detectaron %d coincidencias de \"%s\". ]", *cantidadPalabras, palabraClave );
    
    free( buf );
    
@@ -146,11 +153,11 @@ char * contarPalabraClave( int fdi, int *cantidadPalabras ){
 // Si el puntero es NULL, no imprime los datos de palabraClave.
 void mostrarResultados( int cantidadPalabras, char *palabraClave ){
     
-    if( palabraClave != NULL ){ 
-        // Modo palabra clave.
-        printf( "\nSe detectaron [ %d ] coincidencias de \"%s\".\n", cantidadPalabras, palabraClave );
-    }else{  
-        // Modo palabras totales.
-        printf( "\nSe detectaron [ %d ] palabras.\n", cantidadPalabras );
-    }
+   if( palabraClave != NULL ){ 
+      // Modo palabra clave.
+      printf( "\nSe detectaron [ %d ] coincidencias de \"%s\".\n", cantidadPalabras, palabraClave );
+   }else{  
+      // Modo palabras totales.
+      printf( "\nSe detectaron [ %d ] palabras.\n", cantidadPalabras );
+   }
 }
--- Y_01/Info_01/Practicas/2013/11-Archivos/11.1-Contar_palabras/main.c
+++ Y_01/Info_01/Practicas/2013/11-Archivos/11.1-Contar_palabras/main.c
@@ -10,52 +10,53 @@
 
 
 int main( int argc, char *argv[] ){
-    int fdi = 0;
-    int opcion;
-    int cantidadPalabras = 0;
-    char *palabraClave = NULL;
-    
-    // Array de punteros a función:
-    char * (* seleccion[ 2 ])( int fdi, int *cantidadPalabras ) = { contarPalabras, contarPalabraClave };
-    
-    if( argc == 2 ){
-        fdi = open( argv[ 1 ], O_RDONLY );
-        
-        if( fdi > 0 ){
-            printf( "Seleccione el tipo de comportamiento:\n"
-                    "1) Cuenta palabras.\n"
-                    "2) Cuenta cuantas veces aparece una palabra clave (coincidencia exacta).\n" );
+   int fdi = 0;
+   int opcion;
+   int cantidadPalabras = 0;
+   char *palabraClave = NULL;
+
+   // Array de punteros a función:
+   char * (* seleccion[ 2 ])( int fdi, int *cantidadPalabras ) = { contarPalabras, contarPalabraClave };
+
+   if( argc == 2 ){
+      fdi = open( argv[ 1 ], O_RDONLY );
+     
+      if( fdi > 0 ){
+         printf( "Seleccione el tipo de comportamiento:\n"
+                 "1) Cuenta palabras.\n"
+                 "2) Cuenta cuantas veces aparece una palabra clave (coincidencia exacta).\n" );
+         
+         do{ // Input del usuario:
             printf( "\tOpción: " );
             
-            do{ // Input del usuario:
-                scanf( "%d", &opcion );
-                // Flushea buffer.
-                int ch = 0;
-                while( ( ch = getchar() ) != '\n' && ch != EOF );
-                
-                if( opcion < 1 || opcion > 2 ){
-                    printf( "\n[ ERROR: INGRESE UNA OPCIÓN VÁLIDA. ]\n" );
-                    printf( "\tOpción: " );
-                }else{
-                    // Usar puntero a función para elegir lo que hace.
-                    palabraClave = seleccion[ opcion - 1 ]( fdi, &cantidadPalabras );
-                }
-            }while( opcion < 1 || opcion > 2 );
+            scanf( "%d", &opcion );
+            // Flushea buffer.
+            int ch = 0;
+            while( ( ch = getchar() ) != '\n' && ch != EOF );
+
+            if( opcion < 1 || opcion > 2 ){
             
-            mostrarResultados( cantidadPalabras, palabraClave );
-            
-            if( palabraClave != NULL )
-                free( palabraClave );
-            
-            close( fdi );
-        }else{
-            printf( "[ ERROR: NOMBRE DE ARCHIVO INVÁLIDO, NO SE PUDO ABRIR. ]\n" );
-        }
-    }else{
-        printf( "[ ERROR: NÚMERO DE ARGUMENTOS INVÁLIDO. ]\n" );
-    }
-    
-    return 1;
+               printf( "\n[ ERROR: INGRESE UNA OPCIÓN VÁLIDA. ]\n" );
+            }else{
+               // Usar puntero a función para elegir lo que hace.
+               palabraClave = seleccion[ opcion - 1 ]( fdi, &cantidadPalabras );
+            }
+         }while( opcion < 1 || opcion > 2 );
+         
+         mostrarResultados( cantidadPalabras, palabraClave );
+         
+         if( palabraClave != NULL )
+            free( palabraClave );
+         
+         close( fdi );
+      }else{
+         printf( "[ ERROR: NOMBRE DE ARCHIVO INVÁLIDO, NO SE PUDO ABRIR. ]\n" );
+      }
+   }else{
+      printf( "[ ERROR: NÚMERO DE ARGUMENTOS INVÁLIDO. ]\n" );
+   }
+
+   return 1;
 }
 
 
